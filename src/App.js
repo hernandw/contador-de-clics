7496a4fd734aa87a6b9c2e11a6d0f90671e6f719
Williams Hernandez <hernandw@gmail.com>
2022-03-18 17:54:39 -0300

    proyecto y estilos del contador de clics

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
 import freeCodeCampLogo from './img/freecodecamplogo.png'
 
 function App() {
+  const manejarClic = ()=>{
+    console.log('Clic')
+  }
+  const reiniciarContador = () =>{
+    console.log('Reiniciar')
+  }
   return (
-    <div className="App">
+    
+    <div className='App'>
       <div className='freecodecamp-logo-container'>
         <img className='freecodecamp-logo'
         src={freeCodeCampLogo} 
         alt='Logo de freecodecamp'/>
       </div>
       <div className='container-principal'>
-
+          <Contador 
+          numClics='5'
+          />
+          <Boton 
+          texto='Clic'
+          esBotonClic={true}
+          manejarClic={manejarClic}
+          />
+          <Boton 
+          texto='Reiniciar'
+          esBotonClic={false}
+          manejarClic={reiniciarContador}
+          />
       </div>
     </div>
   );
---
+++ src/componentes/Boton.js
@@ -0,0 +1,12 @@
+import './boton.css'
+
+function Boton({texto, esBotonClic, manejarClic}){
+    return (
+        <button className={ esBotonClic ? 'boton-clic' : 'boton-reiniciar' }
+            onClick={manejarClic}>
+            {texto}
+        </button>
+    );
+}
+
+export default Boton;
