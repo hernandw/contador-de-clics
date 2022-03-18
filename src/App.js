add6ad02495900a21bc6f1647bd9a07053cea3d7
Williams Hernandez <hernandw@gmail.com>
2022-03-18 15:51:59 -0300

    inicio del proyecto contador de clics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import freeCodeCampLogo from './img/freecodecamplogo.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='freecodecamp-logo-container'>
+        <img className='freecodecamp-logo'
+        src={freeCodeCampLogo} 
+        alt='Logo de freecodecamp'/>
+      </div>
+      <div className='container-principal'>
+
+      </div>
     </div>
   );
 }
